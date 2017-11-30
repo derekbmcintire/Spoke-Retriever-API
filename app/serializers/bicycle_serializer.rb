03c64678fa4938c18f89348946c9663707199a6b
derekbmcintire <derekbmcintire@gmail.com>
2017-11-30 11:09:03 -0500

    Add editable and user_id to serializer

--- app/serializers/bicycle_serializer.rb
+++ app/serializers/bicycle_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class BicycleSerializer < ActiveModel::Serializer
-  attributes :id, :make, :model, :color, :serial_number, :size, :editable
+  attributes :id, :make, :model, :color, :serial_number, :size, :editable, :user_id
 
   def editable
     scope == object.user

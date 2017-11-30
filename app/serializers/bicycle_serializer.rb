66240e2059c3480360576b50b84feb9516c2ce31
derekbmcintire <derekbmcintire@gmail.com>
2017-11-30 08:34:11 -0500

    Add editable to bicycle serializer

--- app/serializers/bicycle_serializer.rb
+++ app/serializers/bicycle_serializer.rb
@@ -1,7 +1,9 @@
-class BicycleSerializer < ActiveModel::Serializer
-  attributes :id, :make, :model, :color, :serial_number, :size
+# frozen_string_literal: true
 
-  # def editable
-  #   scope == object.user
-  # end
+class BicycleSerializer < ActiveModel::Serializer
+  attributes :id, :make, :model, :color, :serial_number, :size, :editable
+
+  def editable
+    scope == object.user
+  end
 end

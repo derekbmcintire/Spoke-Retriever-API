40db77556e932816f0add5bb3c644b0bc19385aa
derekbmcintire <derekbmcintire@gmail.com>
2017-12-02 23:47:02 -0500

    Add bicycle_id to events table
    - add bicycle references column to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171202160246) do
+ActiveRecord::Schema.define(version: 20171203044552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20171202160246) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "event_type"
+    t.bigint "bicycle_id"
+    t.index ["bicycle_id"], name: "index_events_on_bicycle_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
@@ -56,6 +58,7 @@ ActiveRecord::Schema.define(version: 20171202160246) do
   end
 
   add_foreign_key "bicycles", "users"
+  add_foreign_key "events", "bicycles"
   add_foreign_key "events", "users"
   add_foreign_key "examples", "users"
 end

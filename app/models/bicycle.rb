faa22236fdf8c1c860c73fea01d8c039cde3ae9c
derekbmcintire <derekbmcintire@gmail.com>
2017-12-07 16:39:05 -0500

    Add event dependency in bicycle model
    - add :dependent => destroy to events in bicycle model

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,5 +1,5 @@
 class Bicycle < ApplicationRecord
   belongs_to :user
-  has_many :events
+  has_many :events, :dependent => :destroy
   validates_presence_of :make, :model, :color, :size, :url
 end
